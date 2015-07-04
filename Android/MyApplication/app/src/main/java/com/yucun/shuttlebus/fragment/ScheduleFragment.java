7d05037e8ae4323f92d3e51825c1c8b59f5b4de4
yucun li <liyucun2012@gmail.com>
2015-07-04 00:25:16 -0400

    add menu to switch sgw and loyola

--- Android/MyApplication/app/src/main/java/com/yucun/shuttlebus/fragment/ScheduleFragment.java
+++ Android/MyApplication/app/src/main/java/com/yucun/shuttlebus/fragment/ScheduleFragment.java
@@ -5,6 +5,9 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -285,6 +288,28 @@ public class ScheduleFragment extends Fragment {
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.schedule_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sgw:
+                sgw_listview.setVisibility(View.VISIBLE);
+                loyola_listview.setVisibility(View.INVISIBLE);
+                return true;
+            case R.id.loyola:
+                sgw_listview.setVisibility(View.INVISIBLE);
+                loyola_listview.setVisibility(View.VISIBLE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void loadSundayFromParse() {
