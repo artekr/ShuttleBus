0f969ccf76e4c3c3c4c1f384de34e77a1a9703cb
yucun li <liyucun2012@gmail.com>
2015-07-30 00:47:45 -0400

    change actionbar to toolbar

--- Android/MyApplication/app/src/main/java/com/yucun/shuttlebus/InfoActivity.java
+++ Android/MyApplication/app/src/main/java/com/yucun/shuttlebus/InfoActivity.java
@@ -1,21 +1,30 @@
 package com.yucun.shuttlebus;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by yucunli on 2015-07-11.
  */
-public class InfoActivity extends Activity {
+public class InfoActivity extends AppCompatActivity {
+
+    @InjectView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ButterKnife.inject(this);
 
+        // Set a toolbar to replace the action bar.
+        toolbar.setTitle("Shuttle Bus");
+        toolbar.setNavigationIcon(R.drawable.ic_ab_up_white);
+        setSupportActionBar(toolbar);
     }
 
     @Override
--- Android/MyApplication/app/src/main/java/com/yucun/shuttlebus/MainActivity.java
+++ Android/MyApplication/app/src/main/java/com/yucun/shuttlebus/MainActivity.java
@@ -1,19 +1,27 @@
 package com.yucun.shuttlebus;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.yucun.shuttlebus.fragment.ScheduleFragment;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        // Set a toolbar to replace the action bar.
+        toolbar.setTitle("Shuttle Bus");
+        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
